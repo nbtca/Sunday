4168cce3aa5b965ba001499e9bdf9f5995c73455
templari <q79142466@163.com>
2021-09-06 19:59:14 +0800

    fix vh

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="flex flex-col-reverse	 sm:(flex flex-row) overflow-hidden" style="height: 100vh">
-    <div class=" sm:(w-[17vw] mix-w-[17vw] bg-current)">
-      <Menu></Menu>
+  <div
+    class="flex flex-col-reverse sm:(flex flex-row) overflow-hidden"
+    style="height: 100vh"
+    :style="[isSafari ? '' : 'height: calc(var(---vh, 1vh) * 100)']"
+  >
+    <div class="sm:(w-[17vw] mix-w-[17vw] bg-current)">
+      <Menu ref="menu"></Menu>
     </div>
-    <div class="w-full">
+    <div class="w-full" @click="this.$refs.menu.isOpen = false">
       <router-view v-slot="{ Component }">
         <keep-alive>
           <component :is="Component" />
@@ -17,8 +21,19 @@
 import Menu from "@/components/Menu/Menu.vue";
 export default {
   name: "Index",
+  data() {
+    return {
+      isSafari: false,
+    };
+  },
   components: {
     Menu,
+  },
+  created() {
+    var userAgent = navigator.userAgent;
+    this.isSafari = userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") == -1;
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("---vh", `${vh}px`);
   },
 };
 </script>
